f6bfdf64409759be3508741bf447dd0229aec7e3
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-12-11 15:22:43 +0300

    Set up Random numbers array in App.jsx and used state to pass the value of dice.

--- App.jsx
+++ App.jsx
@@ -1,22 +1,25 @@
+import { useState } from 'react'
 import Die from './components/Die'
 
 const App = () => {
+    const [dice, setDice] = useState(generateAllNewDice())
+    function generateAllNewDice () {
+        const randomArray = []
+        for (let i = 0; i < 10; i++) {
+            const randomIndex = Math.ceil(Math.random() * 6)
+            randomArray.push(randomIndex)
+        }
+        return randomArray
+    }
+   
+
+    const diceArray = dice.map((num) => <Die value={num}/>)
+
   return (
       <main>
-        <div className="die">
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
+        <div className="container">
+            {diceArray}
         </div>
-        
-        
       </main>
   )
 }
